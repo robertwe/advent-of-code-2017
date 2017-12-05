77486f9d71320c877779932a113a822ae5eaebc8
Michael Whatcott <mdwhatcott@gmail.com>
2017-12-04 22:33:24 -0700

    Solved day 5

--- day05/main.go
+++ day05/main.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/mdwhatcott/advent-of-code/util"
+	"github.com/smartystreets/assertions/assert"
+	"github.com/smartystreets/assertions/should"
+)
+
 func main() {
+	fmt.Println(assert.So(part1(), should.Equal, 351282))
+	fmt.Println(assert.So(part2(), should.Equal, 24568703))
+}
+
+func part1() int {
+	program := &Program{ints: util.ParseInts(util.InputLines())}
+	program.Execute()
+	return program.steps
+}
+
+func part2() int {
+	program := &Program{ints: util.ParseInts(util.InputLines())}
+	program.part2 = true
+	program.Execute()
+	return program.steps
 }
--- day05/stuff.go
+++ day05/stuff.go
@@ -1,1 +1,36 @@
 package main
+
+type Program struct {
+	part2   bool
+	counter int
+	steps   int
+	ints    []int
+}
+
+func (this *Program) Execute() {
+	for this.inRange() {
+		c := this.counter
+
+		this.steps++
+
+		current := this.ints[this.counter]
+		this.counter += current
+
+		if c >= len(this.ints) || c < 0 {
+			break
+		}
+		this.increment(c)
+	}
+}
+
+func (this *Program) inRange() bool {
+	return this.counter < len(this.ints) && this.counter >= 0
+}
+
+func (this *Program) increment(c int) {
+	if this.part2 && this.ints[c] >= 3 {
+		this.ints[c]--
+	} else {
+		this.ints[c]++
+	}
+}
