242bad9a939290696a93ac75c710afe04c78da40
Michael Whatcott <mdwhatcott@gmail.com>
2017-12-19 22:04:43 -0700

    Completed day 9 using Rob Pike's approach to lexical scanning.
    https://www.youtube.com/watch?v=HxaD_trXwRE

--- day09/main.go
+++ day09/main.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/mdwhatcott/advent-of-code/util"
+	"github.com/smartystreets/assertions/assert"
+	"github.com/smartystreets/assertions/should"
+)
+
 func main() {
+	fmt.Println("Group score:        ", assert.So(groupScore(util.InputString()), should.Equal, 14212))
+	fmt.Println("Garbage characters: ", assert.So(garbageScore(util.InputString()), should.Equal, 6569))
 }
