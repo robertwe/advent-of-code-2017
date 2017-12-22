c5a75ef641f85ddec0ff9c058359a7495102846b
Michael Whatcott <mdwhatcott@gmail.com>
2017-12-22 16:27:31 -0700

    Solved day 11.

--- day11/main.go
+++ day11/main.go
@@ -1,4 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/mdwhatcott/advent-of-code/util"
+	"github.com/smartystreets/assertions/assert"
+	"github.com/smartystreets/assertions/should"
+)
+
 func main() {
+	var maxDistance int
+	var start Hex
+	var position Hex
+
+	path := strings.Split(util.InputString(), ",")
+
+	for _, direction := range path {
+		position = position.Offset(Offsets[direction])
+		if distance := position.DistanceTo(start); distance > maxDistance {
+			maxDistance = distance
+		}
+	}
+
+	fmt.Println(assert.So(position.DistanceTo(start), should.Equal, 707))
+	fmt.Println(assert.So(maxDistance, should.Equal, 1490))
+}
+
+var Offsets = map[string]Hex{
+	"n":  North,
+	"ne": NorthEast,
+	"nw": NorthWest,
+	"s":  South,
+	"se": SouthEast,
+	"sw": SouthWest,
 }
